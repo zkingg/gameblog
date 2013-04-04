f72ac71ce9b93eab7135eac4c06bcc9e9b8a9683
luffyzbest <luffyzbest@gmail.com>
2013-04-04 21:02:03 +0000

    ajout: gestion catégorie

--- GameBlog/WebContent/js/init_alert.js
+++ GameBlog/WebContent/js/init_alert.js
@@ -46,4 +46,58 @@ $(document).ready(function(){
 				
 		});	
 	});
+
+
+	$(".categorie_delete").click(function(){
+		var id = $(this).attr("data");
+		$(".valid_categorie_delete").attr("data",id);
+		$("#ValidModal").modal('toggle');
+	});
+	
+	$(".valid_categorie_delete").click(function(){
+		var _id = $(this).attr("data");
+		$.ajax({
+			type: 'POST' ,
+			url: '/GameBlog/categorie/delete',
+			data: {
+				id: _id,
+			},
+			success: function(data){location.reload();},
+			error: function(data){alert(data);},
+			complete: function(){ $("#ValidModal").modal('hide'); }
+				
+		});	
+	});
+
+	$(".categorie_edit").click(function(){
+		var id = $(this).attr("data");
+		var categorie = $("#categorie_"+id).html();
+		$("#categorie_"+id).html(
+				'<div class="form-search" style="margin:0px;">'+
+					"<div class='input-append'>"+
+						"<input type='text' class='search-query' id='nom_categorie_"+id+"' value='"+categorie+"'>"+
+						"<div class='btn valid_categorie_edit' data='"+id+"' >Editer</div>"+
+					"</div>"+
+				'</div>'
+		);
+		
+		$('.valid_categorie_edit').bind("click",function(){
+			var _id = $(this).attr("data");
+			var _nom = $("#nom_categorie_"+id).val();
+			
+			$.ajax({
+				type: 'POST' ,
+				url: '/GameBlog/categorie/edit',
+				data: {
+					id: _id,
+					nom: _nom,
+				},
+				success: function(data){},
+				error: function(data){alert(data);},
+				complete: function(){$("#categorie_"+id).html($("#nom_categorie_"+id).val());}
+					
+			});
+		});
+	});
+
 });
--- GameBlog/src/com/core/beans/Categorie.java
+++ GameBlog/src/com/core/beans/Categorie.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import com.core.util.GetConnection;
+import com.core.util.Pagination;
 
 
 public class Categorie {
@@ -53,6 +54,26 @@ public class Categorie {
 		return list;
 	}
 	
+	public static ArrayList<Categorie> getCategories(int page){
+		ArrayList<Categorie> list = new ArrayList<Categorie>();
+		Statement stm = null;
+		try {
+			stm = GetConnection.getConnection().createStatement();
+			ResultSet res = stm.executeQuery("select id from categories limit "+(page-1)*Pagination.ELEMENT_PAR_PAGE+","+Pagination.ELEMENT_PAR_PAGE);
+			while(res.next()){
+				list.add(new Categorie(res.getInt("id")));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {if(stm!=null) stm.close();} 
+			catch (SQLException e) {}
+		}
+		
+		return list;
+	}
+	
 	public static boolean creerCategorie(String nom){
 		PreparedStatement stm =null;
 		try {
@@ -62,12 +83,68 @@ public class Categorie {
 			return true;
 			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
 		}finally{
 			try {if(stm!=null) stm.close();} 
 			catch (SQLException e) {}
 		}
+	}
+	public static int getNbCategories() {
+		Statement stm = null;
+		try {
+			stm = GetConnection.getConnection().createStatement();
+			ResultSet res = stm.executeQuery("select id from categories");
+			res.last();
+			return res.getRow();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally{
+			try {if(stm!=null) stm.close();} 
+			catch (SQLException e) {}
+		}	
+		
+		return 0;
+	}
+	
+	public static boolean delete(int id) {
+		Statement stm = null;
+		try {
+			stm = GetConnection.getConnection().createStatement();
+			if(stm.executeUpdate("delete from categories where id="+id) == 0)
+				return false;
+				
+			stm.executeUpdate("delete from contenu_categorie where categorie_id="+id);//retrait categorie
+				
+			return true;
+		}
+		catch (SQLException e) {e.printStackTrace();}
+		finally{
+			try {if(stm!=null) stm.close();} 
+			catch (SQLException e) {}
+		}
+		
+		return false;
+	}
+	
+	public static boolean edit(int id, String nom) {
+		// TODO Auto-generated method stub
+		PreparedStatement stm = null;
+		
+		try {
+			stm = GetConnection.getConnection().prepareStatement("update categories set nom=? where id=?");
+			stm.setString(1, nom);
+			stm.setInt(2, id);
+			stm.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			try {if(stm!=null) stm.close();} 
+			catch (SQLException e) {}
+		}
+		
+		return false;
 	}
 }

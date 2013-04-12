da72df92113db3bfe2f13e1aa1bb3d0c360a13d1
luffyzbest@gmail.com <luffyzbest@gmail.com@26a622d4-dcc5-7012-431f-02412a5fe817>
2013-04-12 21:14:55 +0000

    mise a jour du backend de gestion du carousel

--- trunk/GameBlog/WebContent/jsp/layout/header.jsp
+++ trunk/GameBlog/WebContent/jsp/layout/header.jsp
@@ -44,8 +44,10 @@
 			</s:iterator>	
 		</div>
 		<!-- Carousel nav -->
-		<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-		<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		<s:if test="carousel.size() > 1">
+			<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+			<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		</s:if>
 	</div>
 	
 	<!--  Popup Message -->
--- trunk/GameBlog/src/com/blog/action/carousel/CarouselAction.java
+++ trunk/GameBlog/src/com/blog/action/carousel/CarouselAction.java
@@ -5,20 +5,24 @@ import java.io.IOException;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
 
 import com.blog.action.TemplateAction;
+import com.core.beans.Article;
 import com.core.beans.CarouselElement;
 import com.core.util.PopupMessage;
 import com.opensymphony.xwork2.ActionContext;
 
-public class CarouselAction extends TemplateAction implements ServletRequestAware{
+public class CarouselAction extends TemplateAction implements ServletRequestAware,ServletResponseAware{
 	private File image;
 	private String imageContentType;
 	private String imageFileName;
 	private HttpServletRequest req;
+	private HttpServletResponse resp;
 	private String titre;
 	private String contenu;
 	private int id;
@@ -38,20 +42,27 @@ public class CarouselAction extends TemplateAction implements ServletRequestAwar
 	public void setId(int id){this.id = id;}
 	@Override
 	public void setServletRequest(HttpServletRequest arg0) {this.req=arg0;}
+	@Override
+	public void setServletResponse(HttpServletResponse arg0) {resp = arg0;}
 	
+	/**
+	 * Action d'edition des informations du carousel
+	 * @return success => retour page précédente
+	 */
 	public String validEdit(){
 		Map session = ActionContext.getContext().getSession();
         try {
-        	String[] tmp = imageFileName.split("\\.");
-        	String extension = tmp[tmp.length-1];
-        	if(!extension.equals("png") && !extension.equals("jpeg") && !extension.equals("gif") && !extension.equals("jpg") )
-        		throw new Exception("Invalide image format !!");
-        		
         	if(image != null){
+	        	String[] tmp = imageFileName.split("\\.");
+	        	String extension = tmp[tmp.length-1];
+	        	if(!extension.equals("png") && !extension.equals("jpeg") && !extension.equals("gif") && !extension.equals("jpg") )
+	        		throw new Exception("Invalide image format !!");
+        		
 				String filePath = req.getSession().getServletContext().getRealPath("/content/carousel/");			
 				File fileToCreate = new File(filePath, this.imageFileName);
 				FileUtils.copyFile(this.image, fileToCreate);
-        	}
+        	}else
+        		imageFileName = "";
 			
 			if(! CarouselElement.edit(id, titre, contenu, imageFileName))
 				throw new Exception("Echec mise a jour de l'element du carousel ...");
@@ -63,5 +74,50 @@ public class CarouselAction extends TemplateAction implements ServletRequestAwar
         
 		return "success";
 	}
-
+	
+	/**
+	 * Action de suppresion d'un element du carousel (Ajax)
+	 * @param id (get/post)
+	 * @return null
+	 */
+	public String delete(){
+		String id = req.getParameter("id");
+		try {
+			if(CarouselElement.delete(Integer.parseInt(id)))	
+				resp.getWriter().write("ok");
+			else
+				resp.getWriter().write("nok");
+		} catch (NumberFormatException e) {
+			try {resp.getWriter().write("nok - id incorrect");}
+			catch (IOException e1) {e1.printStackTrace();}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	public String create(){
+		Map session = ActionContext.getContext().getSession();
+        try {
+        	if(image != null){
+	        	String[] tmp = imageFileName.split("\\.");
+	        	String extension = tmp[tmp.length-1];
+	        	if(!extension.equals("png") && !extension.equals("jpeg") && !extension.equals("gif") && !extension.equals("jpg") )
+	        		throw new Exception("Invalide image format !!");
+        		
+				String filePath = req.getSession().getServletContext().getRealPath("/content/carousel/");			
+				File fileToCreate = new File(filePath, this.imageFileName);
+				FileUtils.copyFile(this.image, fileToCreate);
+        	}else
+        		imageFileName = "";
+			
+			if(! CarouselElement.create( titre, contenu, imageFileName))
+				throw new Exception("Echec creation de l'element du carousel ...");
+		} catch (Exception e) {
+			session.put("message", new PopupMessage(e.getMessage(), "error"));
+		}
+        
+		return "success";
+	}
 }
